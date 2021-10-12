528b9f2d09d6ee3f1b141f024f747459c374eac3
Artyom Ulanchik <artyom.u@outlook.com>
2021-10-13 00:13:48 +0100

    Code clean up

--- src/containers/Pokemons.js
+++ src/containers/Pokemons.js
@@ -3,25 +3,30 @@ import { useQuery, gql } from '@apollo/client'
 import { Card } from '../components'
 
 const Pokemons = () => {
-    const {loading, data : { pokemons = [] } = {}, error} = useQuery(GET_POKEMONS, {
+
+    const { loading, error, data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
         variables: {
             first: 151
         }
     });
-    
-    if (loading) return <div>Loading...</div>
-    if (error) return <div>Error: `${error.message}`</div>
 
-    return (
-        <div className="container">
-            {pokemons && pokemons.map(
-                pokemon => (
-                    <Card key={pokemon.id} pokemon={pokemon} />
-                )
-            )}
-            {error && <span>Unable to get data</span>}
-        </div>
-    )
+    if (loading) return (<div className="container container_text"><p>Loading...</p></div>)
+
+    if (pokemons && pokemons.length > 0) {
+        return (
+            <div className="container">
+                {pokemons.map(pokemon => <Card key={pokemon.id} pokemon={pokemon} />)}
+            </div>
+        )
+    }
+
+    if (error) {
+        return (
+            <div className="container container_text">
+                <p>Error ocurred.${error.message}</p>
+            </div>
+        )
+    }
 }
 
 const GET_POKEMONS = gql`
